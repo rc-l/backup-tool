10b42ada78d9e9cc11aa42a7960325aec336b4fa
unknown <24314861+rc-l@users.noreply.github.com>
2020-01-13 22:01:37 +0100

    file copying and logging

--- backup.py
+++ backup.py
@@ -2,9 +2,29 @@
 Backup tool for windows
 """
 
+#TODO: backup root directory through cli parameter
+#TODO: logging to screen to show progress of script
+#TODO: backup integrity check based on hash sums
+#TODO: cleanup mode to remove files from backup that do not exist in origin
+
+import logging
 import os
 import socket
+from shutil import copy2
 
+
+### LOGGING ###
+logger = logging.getLogger("screenlog")
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('{message}', style='{')
+ch.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+create_log_msg = lambda file, status: "{file:{width}}{status}".format(file=file, status=status, width=os.get_terminal_size()[0]-len(status)-2)
+
+### FUNCTIONS ###
 make_backuppath = lambda x: os.path.join(backupdir, x.replace(":", "", 1))
 
 def backup(origin):
@@ -12,13 +32,23 @@ def backup(origin):
     Put file or folder on the backup medium
     """
     assert type(origin) is str
-    backuppath = make_backuppath(path)
+    backuppath = make_backuppath(origin)
     if os.path.isdir(origin):
         if not os.path.isdir(backuppath):
+            logging.info(origin, "ADD")
             os.makedirs(backuppath)
     else:
-        
+        if os.path.isfile(backuppath):
+            if os.path.getmtime(backuppath) >= os.path.getmtime(origin):
+                logger.debug(create_log_msg(origin, "SKIP"))
+                return
+            else:
+                logger.warning(create_log_msg(origin, "UPDATE"))
+        else:
+            logger.warning(create_log_msg(origin, "ADD"))
+        #copy2(origin, backuppath)
 
+### MAIN PROCESS ###
 backupdrive = "H"
 backupdir = os.path.join(f"{backupdrive}:", "backup_" + socket.gethostname())
 
@@ -36,4 +66,4 @@ for path in paths:
             paths.append(originpath)
             backup(originpath)
         elif os.path.isfile(originpath):
-            pass
+            backup(originpath)
